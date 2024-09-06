8a64c08bf9ed411d5f449de9aca4de7548d598a0
TheRyanMajd <rm97798@uga.edu>
2024-09-06 14:10:42 -0400

    ~: Add Google Tag Manager script to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
+import Script from 'next/script';
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +11,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=GTM-5GWFPTWX"/>
+        <Script id='google-analytics' strategy="afterInteractive">
+         {`
+         <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5GWFPTWX');</script>
+<!-- End Google Tag Manager -->
+         `}
+
+        </Script>
+      <body className={inter.className}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5GWFPTWX"
+height="0" width="0" style={{display:'none', visibility:'hidden'}}></iframe></noscript>
+{children}</body>
+      </head>
+      
     </html>
   );
 }
