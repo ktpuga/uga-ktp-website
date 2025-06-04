f8c2a43b2c025c03aa3341fdd19a86ff4c0de17e
TheRyanMajd <rm97798@uga.edu>
2025-06-04 12:48:41 -0400

    fix webpack errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    webpack(config) {
+    config.resolve.alias['@'] = __dirname;   // <—— alias “@/” → project root
+    return config;
+  },
 };
 
 export default nextConfig;
