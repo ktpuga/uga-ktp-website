df126be0e5d92f9f067d33e939fe0cc862c5fe44
TheRyanMajd <rm97798@uga.edu>
2026-02-16 15:06:38 -0500

    fixed copyright date to fetch current year

--- components/ui/footer.jsx
+++ components/ui/footer.jsx
@@ -1,43 +1,49 @@
-import Link from 'next/link'
-import React, { Component } from 'react'
+import Link from "next/link";
+import React, { Component } from "react";
 export class footer extends Component {
   render() {
     return (
-        <footer className="bg-[#052039] py-6 w-full shrink-0 text-white">
-        <div
-          className="container mx-auto px-4 md:px-6 flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-          <p className="text-xs font-['Source Sans Pro']">&copy; 2025 ΚΘΠ. All rights reserved.</p>
+      <footer className="bg-[#052039] py-6 w-full shrink-0 text-white">
+        <div className="container mx-auto px-4 md:px-6 flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
+          <p className="text-xs font-['Source Sans Pro']">
+            &copy; {new Date().getFullYear()} ΚΘΠ. All rights reserved.
+          </p>
           <nav className="flex gap-4 sm:gap-6">
             <Link
               href="/code-of-conduct"
               className="text-xs hover:underline underline-offset-4 font-['Source Sans Pro']"
-              prefetch={false}>
+              prefetch={false}
+            >
               Code of Conduct
             </Link>
             <Link
               href="/blog"
               className="text-xs hover:underline underline-offset-4 font-['Source Sans Pro']"
-              prefetch={false}>
+              prefetch={false}
+            >
               Blog
             </Link>
             <Link
               href="https://www.instagram.com/ugaktp/"
               target="_Blank"
               className="text-xs hover:underline underline-offset-4 font-['Source Sans Pro']"
-              prefetch={false}>
+              prefetch={false}
+            >
               Instagram
             </Link>
             <Link
-              href="https://www.linkedin.com/company/kappa-theta-pi-uga/" target="_blank"
+              href="https://www.linkedin.com/company/kappa-theta-pi-uga/"
+              target="_blank"
               className="text-xs hover:underline underline-offset-4 font-['Source Sans Pro']"
-              prefetch={false}>
+              prefetch={false}
+            >
               LinkedIn
             </Link>
           </nav>
         </div>
       </footer>
-    )
+    );
   }
 }
 
-export default footer
+export default footer;
